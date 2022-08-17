2e1b1a7c4f22b702365470283d61969f945e94aa
jpindyk <jakub.pindyk@gmail.com>
2022-08-17 11:19:03 +0200

    PostMapper added

---
+++ src/main/java/com/blogapplication/mapper/PostMapper.java
@@ -0,0 +1,34 @@
+package com.blogapplication.mapper;
+
+import com.blogapplication.model.Post;
+import com.blogapplication.payload.PostDto;
+
+public class PostMapper {
+
+    public static PostDto mapToPostDto (Post post) {
+        return  PostDto.builder()
+                .id(post.getId())
+                .title(post.getTitle())
+                .url(post.getUrl())
+                .content(post.getContent())
+                .description(post.getDescription())
+                .createdOn(post.getCreatedOn())
+                .updatedOn(post.getUpdatedOn())
+                .build();
+
+    }
+
+    public static Post mapToPost (PostDto postDto) {
+        return  Post.builder()
+                .id(postDto.getId())
+                .title(postDto.getTitle())
+                .url(postDto.getUrl())
+                .content(postDto.getContent())
+                .description(postDto.getDescription())
+                .createdOn(postDto.getCreatedOn())
+                .updatedOn(postDto.getUpdatedOn())
+                .build();
+
+    }
+
+}
