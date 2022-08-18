c63fba813fe1543597cff6e694825aa6e6c4b58e
jpindyk <jakub.pindyk@gmail.com>
2022-08-18 22:14:21 +0200

    minor modification

--- src/main/java/com/blogapplication/service/PostService.java
+++ src/main/java/com/blogapplication/service/PostService.java
@@ -1,8 +1,10 @@
 package com.blogapplication.service;
 
 import com.blogapplication.payload.PostDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 
 public interface PostService {
     List<PostDto> findAllPosts();
--- src/main/java/com/blogapplication/service/PostServiceImplementation.java
+++ src/main/java/com/blogapplication/service/PostServiceImplementation.java
@@ -3,10 +3,12 @@ package com.blogapplication.service;
 import com.blogapplication.mapper.PostMapper;
 import com.blogapplication.payload.PostDto;
 import com.blogapplication.repository.PostRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Service
 public class PostServiceImplementation implements PostService{
 
     private PostRepository repository;
