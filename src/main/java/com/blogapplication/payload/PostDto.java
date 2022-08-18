c579aefce01aa348ed63264bce0e46b5f195ac3e
jpindyk <jakub.pindyk@gmail.com>
2022-08-18 23:28:56 +0200

    constructors

--- src/main/java/com/blogapplication/payload/PostDto.java
+++ src/main/java/com/blogapplication/payload/PostDto.java
@@ -1,12 +1,16 @@
 package com.blogapplication.payload;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class PostDto {
 
     private Long id;
