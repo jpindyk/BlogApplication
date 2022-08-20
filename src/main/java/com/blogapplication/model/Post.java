6eee4193edd1166cb38913941540a9f253bffe8e
jpindyk <jakub.pindyk@gmail.com>
2022-08-20 23:45:49 +0200

    mapping with Comment

--- src/main/java/com/blogapplication/model/Post.java
+++ src/main/java/com/blogapplication/model/Post.java
@@ -11,6 +11,8 @@ import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Data
@@ -23,15 +25,22 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     @NotBlank
+    @Column(nullable = false)
     private String title;
     private String url;
     @Lob
     @NotBlank
+    @Column(nullable = false)
     private String content;
+    @NotBlank
+    @Column(nullable = false)
     private String description;
     @CreationTimestamp
     private LocalDateTime createdOn;
     @UpdateTimestamp
     private LocalDateTime updatedOn;
+
+    @OneToMany(mappedBy = "post", cascade = CascadeType.REMOVE)
+    Set<Comment> commentSet = new HashSet<>();
 
 }
