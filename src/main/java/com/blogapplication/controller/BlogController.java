49f24e9654faf513d27e291e990e19de13b2dbad
jpindyk <jakub.pindyk@gmail.com>
2022-08-20 23:08:02 +0200

    BlogController added

---
+++ src/main/java/com/blogapplication/controller/BlogController.java
@@ -0,0 +1,44 @@
+package com.blogapplication.controller;
+
+import com.blogapplication.model.Post;
+import com.blogapplication.payload.PostDto;
+import com.blogapplication.service.PostService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+public class BlogController {
+    private PostService postService;
+
+    public BlogController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping("/")
+    public String viewBlogPosts(Model model) {
+       List<PostDto> posts =  postService.findAllPosts();
+       model.addAttribute("postsResponse", posts);
+       return "blog/view_posts";
+    }
+
+    @GetMapping("/post/{postUrl}")
+    private String showPost(@PathVariable("postUrl") String postUrl,
+                            Model model){
+        PostDto post = postService.findPostByUrl(postUrl);
+        model.addAttribute("post", post);
+        return "blog/blog_post";
+    }
+    @GetMapping("/page/search")
+    public String searchPosts(@RequestParam(value = "query") String query,
+                              Model model){
+        List<PostDto> postResponse = postService.searchPost(query);
+        model.addAttribute("postsResponse", postResponse);
+        return "blog/view_posts";
+    }
+
+}
